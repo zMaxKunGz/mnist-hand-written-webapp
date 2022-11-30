031e11c528afbedbb71c7f46880026e8c215ba54
Pasit Tiwawongrut <maxkung55@gmail.com>
2022-11-30 16:16:09 +0700

    Added server image resize transform

--- classification_model/server.py
+++ classification_model/server.py
@@ -1,17 +1,28 @@
 from flask import Flask, request
+from flask_cors import CORS
 import torch
+import torchvision.transforms as T
+from torchvision.transforms import InterpolationMode
 from model import Model
 
 app = Flask(__name__)
+CORS(app)
 model = Model()
 model.load_state_dict(torch.load("model.pt"))
 model.eval()
 
-@app.route('/classify')
+@app.route('/classify', methods=["PUT"])
 def hello():
   body = request.get_json()
   data = torch.tensor([[body['data']]])
-  predict = model(data)
+  transforms = torch.nn.Sequential(
+    T.GaussianBlur(5),
+    T.Resize((28,28), interpolation=InterpolationMode.BILINEAR)
+  )
+  resize = transforms(data)
+  predict = model(resize)
   target_class = torch.argmax(predict)
-  print(target_class)
-  return { 'predict': target_class.tolist()}, 200
+  return { 
+    'predict': target_class.tolist(),
+    'image': resize[0].tolist()
+  }, 200
